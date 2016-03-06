e5521e5d557e8d2d7140a663419986a61b905dca
nplusp <nick.pridorozhko@gmail.com>
2016-03-06 12:13:04 +0300

    [Curiosity] positioning

--- lib/curiosity.rb
+++ lib/curiosity.rb
@@ -1,7 +1,16 @@
 class Curiosity
-  attr_accessor :status
+  attr_accessor :x, :y, :facing
 
-  def initialize
-    @status = :waiting
+  def initialize(x: 1, y: 1, facing: 'N')
+    @x = Integer(x)
+    @y = Integer(y)
+    validate_facing(facing)
+    @facing = facing
+  end
+
+  private
+
+  def validate_facing(facing = @facing)
+    raise ArgumentError.new("Invalid facing") unless ["N", "E", "S", "W"].include? facing
   end
 end
--- spec/curiosity_spec.rb
+++ spec/curiosity_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe Curiosity do
-  let(:curiosity) { Curiosity.new }
+  describe "adding curiosity" do
+    context "valid behavior" do
+      it "has default position and facing" do
+        curiosity = Curiosity.new
+        expect(curiosity.x).to eql 1
+        expect(curiosity.y).to eql 1
+        expect(curiosity.facing).to eql 'N'
+      end
 
-  context "initial state" do
-    it "has initial status" do
-      expect(curiosity.status).to eql :waiting
+      it "has custom position and facing" do
+        curiosity = Curiosity.new(x: 2, y: 2, facing: 'S')
+        expect(curiosity.x).to eql 2
+        expect(curiosity.y).to eql 2
+        expect(curiosity.facing).to eql 'S'
+      end
+    end
+
+    context "invalid" do
+      it "is invalid position" do
+        expect { Curiosity.new(x: "Some", y: "Strange") }.to raise_error(ArgumentError)
+      end
+
+      it "is invalid facing" do
+        expect { Curiosity.new(facing: "invalid") }.to raise_error(ArgumentError, "Invalid facing")
+      end
     end
   end
 end
